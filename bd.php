10e61aa5dbd04a75b05104adda5cf1f0181c8ad8
Shayaros <150328065+Shayaros@users.noreply.github.com>
2024-09-13 17:39:15 +0300

    Add files via upload

---
+++ bd.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `bd`
+ */
+
+/* `bd`.`messages` */
+$messages = array(
+  array('message_id' => '1','message_text' => 'Привіт','user_id' => '1'),
+  array('message_id' => '2','message_text' => 'Як справи?','user_id' => '1'),
+  array('message_id' => '10','message_text' => 'english_or_spanish','user_id' => '3'),
+  array('message_id' => '11','message_text' => 'Все добре','user_id' => '2')
+);
+
+/* `bd`.`users` */
+$users = array(
+  array('user_id' => '1','user_name' => 'Андрій'),
+  array('user_id' => '2','user_name' => 'Таня'),
+  array('user_id' => '3','user_name' => 'yaroslav')
+);
